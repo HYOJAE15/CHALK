4a2a75dbcd4d5d027b284aa2e9cc4a5ac38c72a2
bhyun-kim <kimbh.mail@gmail.com>
2023-05-02 14:23:50 +0900

    update etc

--- preprocessing/add_positive_area_from_mask_output.py
+++ preprocessing/add_positive_area_from_mask_output.py
@@ -9,14 +9,14 @@ import cv2
 from glob import glob
 
 
-LABEL_DIR = r'\\172.16.113.151\UOS-SSaS Dropbox\05. Data\02. Training&Test\025. Seoul_Facilities_Corporation\Tancheon-P46-48-Ortho\v0.1.7 - Copy (2)\gtFine\test'
+LABEL_DIR = r'\\172.16.113.151\UOS-SSaS Dropbox\05. Data\02. Training&Test\025. Seoul_Facilities_Corporation\Tancheon-P46-48-Ortho\v0.1.7 - Copy\gtFine\test'
 LABEL_SUFFIX = '_gtFine_labelIds.png'
 
-MASK_DIR = r'\\172.16.113.151\UOS-SSaS Dropbox\05. Data\01. Under_Process\025. Seoul Facility Corporation\2023.04.03 Concrete Crack Detection Cascade Mask R-CNN only Positive Samples'
+MASK_DIR = r'\\172.16.113.151\UOS-SSaS Dropbox\05. Data\01. Under_Process\025. Seoul Facility Corporation\2023.04.12 Concrete Crack Detection Cascade Mask R-CNN only Positive Samples\v1'
 MASK_SUFFIX = '_add_positive.png'
 
-CM_DIR = r'\\172.16.113.151\UOS-SSaS Dropbox\05. Data\01. Under_Process\025. Seoul Facility Corporation\2023.04.03 Concrete Crack Detection Cascade Mask R-CNN only Positive Samples'
-CM_SUFFIX = '_cm_map.png'
+MO_DIR = r'\\172.16.113.151\UOS-SSaS Dropbox\05. Data\01. Under_Process\025. Seoul Facility Corporation\2023.04.12 Concrete Crack Detection Cascade Mask R-CNN only Positive Samples\v1'
+MO_SUFFIX = '_mask_output.png'
 
 NEW_LABEL_DIR = r'\\172.16.113.151\UOS-SSaS Dropbox\05. Data\02. Training&Test\025. Seoul_Facilities_Corporation\Tancheon-P46-48-Ortho\v0.1.8\gtFine\test'
 
@@ -34,17 +34,17 @@ def main():
         # get mask path
         mask_path = os.path.join(MASK_DIR, label_name + MASK_SUFFIX)
         # get confusion matrix path
-        cm_path = os.path.join(CM_DIR, label_name + CM_SUFFIX)
+        mo_path = os.path.join(MO_DIR, label_name + MO_SUFFIX)
 
         # read label
         label = cv2.imread(label_path, cv2.IMREAD_UNCHANGED)
         # read mask
         mask = cv2.imread(mask_path, cv2.IMREAD_UNCHANGED)
         # read confusion matrix
-        cm = cv2.imread(cm_path, cv2.IMREAD_UNCHANGED)
+        mo = cv2.imread(mo_path, cv2.IMREAD_UNCHANGED)
 
         # get indices of green area in cm (RGB) 
-        green_area = np.all(cm == [0, 255, 0], axis=-1)
+        # green_area = np.all(cm == [0, 255, 0], axis=-1)
 
         mask_0 = mask[:, :, 0]
         mask_1 = mask[:, :, 1]
@@ -63,7 +63,7 @@ def main():
         del mask_0, mask_1, mask_2, mask_3, _mask
 
         # intersection of mask and cm 
-        intersection = np.logical_and(mask, green_area)
+        intersection = np.logical_and(mask, mo)
 
         # print number of intersection pixels
         print('label_name: ', label_name)
@@ -72,7 +72,7 @@ def main():
         # add intersection to label
         label[intersection] = 1
 
-        del mask, green_area, intersection
+        del mask, mo, intersection
 
         # save label
         new_label_path = os.path.join(NEW_LABEL_DIR, label_name + LABEL_SUFFIX)
